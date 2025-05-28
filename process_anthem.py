86f5a58173b454b796e31342b01503fd6274c68a
Ethan <mooney.ethan@gmail.com>
2025-05-27 20:33:47 -0600

    modifying the approach to stream the download directly to S3 to conserve memory on airflow

--- process_anthem.py
+++ process_anthem.py
@@ -4,7 +4,6 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from datetime import datetime, timedelta
 import requests
 import boto3
-from io import BytesIO
 
 # Default arguments for the DAG
 default_args = {
@@ -22,9 +21,9 @@ default_args = {
 def process_anthem():
 
     @task
-    def download_and_upload_file():
+    def download_and_upload_file_directly():
         """
-        Downloads a file from a URL and uploads it directly to an S3 bucket.
+        Downloads a file from a URL and streams it directly to an S3 bucket.
 
         Args:
             None
@@ -35,15 +34,16 @@ def process_anthem():
         url = 'https://antm-pt-prod-dataz-nogbd-nophi-us-east1.s3.amazonaws.com/anthem/2025-05-01_anthem_index.json.gz'
         s3_bucket = 'price-transparency-raw'
         s3_key = 'payer/anthem/index_files/main-index/2025-05-01_anthem_index.json.gz'
-        
-        response = requests.get(url, stream=True)
-        if response.status_code == 200:
-            s3_client = boto3.client('s3')
-            s3_client.upload_fileobj(BytesIO(response.content), s3_bucket, s3_key)
-            print(f"✅ Uploaded to s3://{s3_bucket}/{s3_key}")
-        else:
-            raise Exception(f"Failed to download file, status code: {response.status_code}")
 
-    download_and_upload_file()
+        with requests.get(url, stream=True) as response:
+            if response.status_code == 200:
+                s3_client = boto3.client('s3')
+                # Stream the download directly to S3
+                s3_client.upload_fileobj(response.raw, s3_bucket, s3_key)
+                print(f"✅ Uploaded to s3://{s3_bucket}/{s3_key}")
+            else:
+                raise Exception(f"Failed to download file, status code: {response.status_code}")
+
+    download_and_upload_file_directly()
 
 dag = process_anthem()
